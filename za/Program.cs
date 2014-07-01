bccf2e5df2af6871fb9a765fb3dad5c7ef43b299
Олег Штоколов <zab.zabil2012@yandex.ru>
2014-07-01 15:49:10 +0400

    Update Program.cs

--- za/Program.cs
+++ za/Program.cs
@@ -8,7 +8,7 @@ namespace za
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
-			Console.WriteLine ("Hello, World!");
+			Console.WriteLine ("Hello World!");
 		}
 	}
 }
